ea0bab682ee4ec1453cbb5c9beed74bff9eec2ae
Guilherme Vieira dos Anjos <guilhermevieiradosanjos02@gmailcom>
2024-09-10 12:16:00 -0300

    Alterando o index.ts, para solucionar erro

--- src/http/index.ts
+++ src/http/index.ts
@@ -6,10 +6,10 @@ async function obterDadosUrl<T>(url:string) {
   return resposta.json() as T;
 }
 
-export async function obterCategorias(): Promise<ICategoria> {
-  return obterDadosUrl<ICategoria>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json')
+export async function obterCategorias(): Promise<ICategoria[]> {
+  return obterDadosUrl<ICategoria[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json')
 }
 
-export async function obterReceitas(): Promise<IReceita> {
-  return obterDadosUrl<IReceita>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json')
+export async function obterReceitas(): Promise<IReceita[]> {
+  return obterDadosUrl<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json')
 }
